d6138fac1eba30ce9356003019b7f679f7b2b44d
Asif <kaifkhan2981@gmail.com>
2026-08-19 18:00:12 +0500

    feat: add script to erase text regions in product images using linear interpolation

--- scratch/test_erase.py
+++ scratch/test_erase.py
@@ -1,30 +1,33 @@
 from PIL import Image
-import numpy as np
 
 img_path = r"public\images\huda-essence-dior-sauvage.jpg"
 out_path = r"scratch\test_erased.jpg"
 
-img = Image.open(img_path)
-data = np.array(img)
+img = Image.open(img_path).convert("RGB")
+width, height = img.size
 
-# We want to erase the text in y range [750, 950] and x range [280, 616]
-# Let's do horizontal interpolation to fill this box from the sides
+# We want to erase the text in y range [800, 950] and x range [280, 616]
 y1, y2 = 800, 950
 x1, x2 = 280, 616
 
+pixels = img.load()
+
 for y in range(y1, y2):
     # Left reference pixel color
-    left_color = data[y, x1 - 1].astype(float)
+    r_l, g_l, b_l = pixels[x1 - 15, y]
     # Right reference pixel color
-    right_color = data[y, x2 + 1].astype(float)
+    r_r, g_r, b_r = pixels[x2 + 15, y]
     
-    # Interpolate
-    width = x2 - x1 + 1
+    span = x2 - x1 + 1
     for x in range(x1, x2 + 1):
-        t = (x - x1) / width
-        color = (1 - t) * left_color + t * right_color
-        data[y, x] = color.astype(np.uint8)
+        t = (x - x1) / span
+        r = int((1 - t) * r_l + t * r_r)
+        g = int((1 - t) * r_l + t * r_r)  # Keep grey scale consistent if needed, but let's use the colors
+        # Let's use the actual colors:
+        r = int((1 - t) * r_l + t * r_r)
+        g = int((1 - t) * g_l + t * g_r)
+        b = int((1 - t) * b_l + t * b_r)
+        pixels[x, y] = (r, g, b)
 
-erased_img = Image.fromarray(data)
-erased_img.save(out_path)
-print("Erase test completed!")
+img.save(out_path)
+print("Erase test with pure PIL completed!")
